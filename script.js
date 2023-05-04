8cdd7b319d17e9e40c29e3b0751f763a6dac6010
Jeff Perrin <jeffreyperrin10@gmail.com>
2023-05-04 16:29:51 -0400

    Add semi-functioning calculator GUI. No decimal functionality. Not able to evaluate pair of #s before pressing =

--- script.js
+++ script.js
@@ -1,5 +1,90 @@
+const display = document.querySelector('#display');
+display.textContent = '0';
+const buttons = document.querySelectorAll('button');
+const digits = document.querySelectorAll('.digit');
+digits.forEach((digit) => {
+    digit.addEventListener('click', () => {
+        //if ()
+        if (display.textContent === '0') {
+            display.textContent = '';
+            //operator no longer selected;
+        } else (operations.forEach((operation) => {
+            if (operation.classList.length > 1) {
+                display.textContent = '';
+                unselectButtons(buttons);
+            };
+        }));
+    
+        display.textContent += digit.textContent;
+    });
+});
+
+
+let num1;// Populated by number entered
+let num2 = '';// Populated by number entered
+let operator = '';
+let result;
+const operations = document.querySelectorAll('.operator');
+
+operations.forEach((operation) => {
+    operation.addEventListener('click', () => {
+        unselectButtons(buttons);
+        operator = operation.id;
+        operation.classList.toggle('selected');
+        num1 = display.textContent;
+    });
+});
+
+function unselectButtons(buttons) {
+    buttons.forEach((button) => {
+        if (button.classList.length > 1) {
+            button.classList.toggle('selected');
+        };
+    });
+};
+
+
+
+const equals = document.querySelector('#equals');
+equals.addEventListener('click', () => {
+    // toggle selected off for all buttons not just operators
+    if (equals.classList.length > 1) {
+        equals.classList.toggle('selected');
+        num1 = display.textContent;  
+    } else if (num2 === '') {
+        num2 = display.textContent;
+    } else {
+       
+        num2 = display.textContent;
+    };
+    display.textContent = operate(operator, num1, num2);
+    equals.classList.toggle('selected');
+    //num2 = '';
+})
+
+
+function operate(operator, num1, num2) {
+    console.log(`${num1} ${operator} ${num2}  `)
+    switch (operator) {
+     case 'addition':
+          return add(num1, num2);
+      case 'subtraction':
+          return subtract(num1, num2);
+     case 'multiplication':
+         return multiply(num1, num2);
+      case 'division':
+          return divide(num1, num2)
+        };
+    
+};
+
+
+const clear = document.querySelector('#clear');
+clear.addEventListener('click', clearCalc);
+
+
 function add(a, b) {
-    return a + b;
+    return Number(a) + Number(b);
 };
 
 function subtract(a, b) {
@@ -14,47 +99,20 @@ function divide(a, b) {
     return a / b;
 };
 
-let num1 = 10;// Populated by number entered
-let operator = 'division';// Populated by operator pressed
-let num2 = 12;// Populated by number entered
 
-function operate(operator, num1, num2) {
-    // num1 is value on display before entering operator (What is entered or previous result)
-    // num2 is value on display (Either clicked/typed)
-    // operator is basic math operator (Either clicked/typed)
-    
-    switch (operator) {
-     case 'addition':
-          result = add(num1, num2);
-          break;
-      case 'subtraction':
-          result = subtract(num1, num2);
-          break;
-     case 'multiplication':
-         result = multiply(num1, num2);
-         break;
-      case 'division':
-          result = divide(num1, num2)
-        };
-    return result;
+function clearCalc() {
+    //Erase all data (e.g created elements)
+    unselectButtons(buttons);
+    display.textContent = '0';
+    num1 = '';
+    num2 = '';
+    operator = ''
 };
 
-function divisionClicked() {
-    operator = 'division';
+function changeDisplaySign() {
+    display.textContent *= -1;
 };
 
-function multiplicationClicked() {
-    operator = 'multiplication';
+function makeDisplayPercentage() {
+    display.textContent *= .01;
 };
-
-function subtractionClicked() {
-    operator = 'subtraction';
-};
-
-function additionClicked() {
-    operator = 'addition';
-};
-function equalsClicked() {
-    display = operate(operator, num1, num2);
-    //Should this return anything?
-}
